090973a8832e0fe13bf86c3cb9f3e69162f156c4
Abdulwahdih2005 <abdulwahidh@2005>
2026-01-31 09:58:10 +0530

    Refactor signup process to save user before generating token and improve error handling for MongoDB connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,9 @@ dotenv.config(); // load .env first
 
 export const connectDB = async () => {
   try {
+    const  { MONGO_URL } = process.env;
+    if(!MONGO_URL)  throw new Error("MONGO_URL IS NOT SET")
+
     const conn = await mongoose.connect(process.env.MONGO_URL);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
